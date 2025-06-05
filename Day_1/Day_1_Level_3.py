7fc7cbe14f00a3142fefef1774cc27cd07f15d5d
UP240373 <up240373@alumnos.upa.edu.mx>
2025-06-05 10:37:54 -0600

    Creacion de carpeta y documentacion del dia 1 y 2

---
+++ Day_1/Day_1_Level_3.py
@@ -0,0 +1,60 @@
+
+#       Ejercicios 1
+
+N = int(10)
+F = float(12.5234)
+C = complex(4 + 2j)
+
+S = 'martin emanuel es joto y estupido'
+B = True
+L = [1, 2, 3, 4, 5, 6] 
+T = ('Angel', 'Juan', 'Gabriel')
+Se = {10, 8, 6, 4, 2, 0}
+D = {
+'paco':'angel',
+'G':'gomez',
+'Ta':'JP', 
+'age':250, 
+'is_married':True,
+'skills':['JS', 'React', 'Node', 'Python']
+}
+
+
+
+print("")
+print("")
+print("")
+
+print(type(N))
+print(type(F))
+print(type(C))
+
+print(type(S))
+print(type(B))
+print(type(L))
+print(type(T))
+print(type(Se))
+print(type(D))
+
+
+print("")
+print("")
+print("")
+
+
+#       Ejercicios 2
+
+x1 = float (input("Ingresa el valor de x1 \n"))
+y1 = float (input("Ingresa el valor de y1 \n"))
+x2 = float (input("Ingresa el valor de x2 \n"))
+y2 = float (input("Ingresa el valor de y2 \n"))
+print("")
+
+print(((x2 - x1)**2) + ((y2 - y1)**2))
+
+print("")
+print("")
+print("")
+
+
+
---
+++ Hello.py
@@ -0,0 +1,46 @@
+print("Hello wold")         #Comando para imprimir en terminal
+print("")
+print("")
+print("")
+
+""" Prueba para usar operacion en python
+"""
+
+print("1 + 2 = ", 1 + 2, "  # Suma (+)")
+print("3 - 2 = ", 3 - 2, "  # Resta (-)")
+print("4 * 2 = ", 4 * 2, "  # Multiplicacion (*)")
+print("3 / 2 = ", 3 / 2, "  # Division (/)")
+print("5 % 3 = ", 5 % 3, "  # Encontrar el residuo de una division (%)")
+print("9 // 3 = ", 9 // 3, "  # Encontrar cuantas veces se puede dividir el numero (//)")
+print("")
+print("")
+print("")
+
+""" Tipos de variables en Python
+"""
+
+N = 1000
+print("Numeros enteros (int) = ", type(N))
+
+F = 10.132
+print("Numeros flotantess (float) = ", type(F))
+
+print("Numeros complejos (complex) = ", type(1 + 3j))
+
+ABD = 'hola mundo'
+print("Cada de caracteres (String) = ", type(ABD))
+
+LED = True 
+print("Numeros booleanos (Boolean-true) = ", type(LED))
+
+print("Lista (list) = ", type([1, 2, 3]))
+
+print("Dictionario (dictionary) = ", type(['name', 'like']))
+
+print("Set (set) = ", type({9.8, 3.14, 2.7}))
+
+print("Tuple (tuple) = ", type((9.8, 3.14, 2.7)))
+
+print("")
+print("")
+print("")

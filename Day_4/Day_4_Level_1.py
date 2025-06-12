762fce564591e1a782912ea7b37892df5e5e1bb1
UP240373 <up240373@alumnos.upa.edu.mx>
2025-06-12 09:03:44 -0600

    Avance de ejercicios del 18 al 24

--- Day_4/Day_4_Level_1.py
+++ Day_4/Day_4_Level_1.py
@@ -101,13 +101,71 @@ print("")
 #       Ejercicios 17
 
 print('Caracter num 10:',company[10])
+
+print("")
+print("")
 print("")
 
 
+#       Ejercicios 18
+
+company = 'Python For Everyone'
+text1 = company[0:2] + company[7:10] + company[11:14]
+print('Acronimo de \"Python For Everyone\": ', text1)
+print("")
 
 
+#       Ejercicios 19
+
+company = 'Coding For All'
+text1 = company[0:3] + company[7] + company[11:14]
+print('Acronimo de \"Coding For All\": ', text1)
+
+print('\n\n')
 
 
+#       Ejercicios 20
+
+company = 'Coding For All'
+text1 = 'C'
+
+print(company.index(text1))  
+print("")
+
+
+#       Ejercicios 21
+
+text1 = 'F'
+
+print(company.index(text1))  
+print("")
+
+
+#       Ejercicios 22
+
+company = 'Coding For All People'
+text1 = 'l'
+
+print(company.rfind(text1))  
+
+print('\n\n')
+
+
+#       Ejercicios 23
+
+company = 'You cannot end a sentence with because because because is a conjunction'
+text1 = 'because'
+
+print('Primera posicion del string con la palabra \"because\": ', company.find(text1))  
+print("")
+
+
+#       Ejercicios 24
+
+company = 'You cannot end a sentence with because because because is a conjunction'
+text1 = 'because'
+
+print('Ultima posicion del string con la palabra \"because\": ', company.rfind(text1))  
 
 
 

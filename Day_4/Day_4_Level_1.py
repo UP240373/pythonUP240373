0fcd96e644fe984226addca6235aa0ce60b84aa7
UP240373 <up240373@alumnos.upa.edu.mx>
2025-06-11 19:08:05 -0600

    Inicio del dia 4 (Avances hasta el ejercicio 4)

---
+++ Day_4/Day_4_Level_1.py
@@ -0,0 +1,93 @@
+
+#       Ejercicios 1 
+
+text1 = 'Thirty'
+text2 = 'Days' 
+text3 = 'Of'
+text4 = 'Python' 
+space = ' '
+
+text_complete = text1 + space + text2 + space + text3 + space + text4
+print(text1)
+print(text2)
+print(text3)
+print(text4)
+print(text_complete)
+
+print('\n\n')
+
+
+#       Ejercicios 3
+
+company = 'Coding For All'
+
+
+#       Ejercicios 4
+
+print(company)
+
+
+#       Ejercicios 5 
+
+print(len(company))
+
+print('\n\n')
+
+
+#       Ejercicios 6 
+
+print(company.upper())
+
+
+#       Ejercicios 7 
+
+print(company.lower())
+
+print('\n\n')
+
+
+#       Ejercicios 8 
+
+print(company.title())
+
+
+#       Ejercicios 9 
+
+print(company.strip('Coding'))
+
+
+#       Ejercicios 10 
+
+print(company.startswith('Coding'))
+
+
+#       Ejercicios 11
+
+company = company.replace('Coding', 'Python')
+print(company)
+
+
+#       Ejercicios 12
+
+print(company.replace('Python', 'Everyone to Python'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

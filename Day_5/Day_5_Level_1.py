c0ac4ac7aff314566bde47437a549c4070b9dc61
UP240373 <up240373@alumnos.upa.edu.mx>
2025-06-12 19:39:59 -0600

    Inicio del dia 5 y avance del nivel 1 (avance del ejercicio 1 al 9)

---
+++ Day_5/Day_5_Level_1.py
@@ -0,0 +1,82 @@
+
+print('\n\n')
+
+#       Ejercicios 1
+
+lst = list()
+
+
+#       Ejercicios 2
+
+materia = ['Programacion', 'Calculo', 'Probabilidad', 'Ingles', 'Sistemas']
+print(materia)
+
+print("")
+
+
+#       Ejercicios 3
+
+print(len(materia))
+
+print('\n\n')
+
+
+#       Ejercicios 4
+
+print(materia[0])
+print(materia[2])
+print(materia[4])
+
+print('\n\n')
+
+
+#       Ejercicios 5
+
+mixed_data_types = ['Abdul', 18, 85, True, 'Aguascalientes, Ags']
+
+
+#       Ejercicios 6
+
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+
+#       Ejercicios 7
+
+print(it_companies)
+print("")
+
+
+#       Ejercicios 8
+
+print(len(it_companies))
+print("")
+
+
+#       Ejercicios 9
+
+print(it_companies[0])
+print(it_companies[3])
+print(it_companies[6])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

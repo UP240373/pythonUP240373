89daeb2983462a26dd206c9beca5e34baa19fcb2
UP240373 <up240373@alumnos.upa.edu.mx>
2025-06-30 18:09:00 -0600

    Dia 7 nivel 2

---
+++ Day_7_Level_2.py
@@ -0,0 +1,70 @@
+
+print('\n\n')
+
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+
+#       Ejercicios 1
+
+print(A.union(B))
+
+print()
+
+
+#       Ejercicios 2
+
+print(A.intersection(B))
+
+print()
+
+
+#       Ejercicios 3
+
+print(A.issubset(B))
+
+print()
+
+
+#       Ejercicios 4
+
+print(A.isdisjoint(B))
+
+print()
+
+
+#       Ejercicios 5
+
+print(A.union(B))
+print(B.union(A))
+
+print()
+
+
+#       Ejercicios 6
+
+print(A.symmetric_difference(B))
+
+print()
+
+
+#       Ejercicios 7
+
+del A, B
+
+
+
+
+
+
+
+
+
+
+
+
+
+
